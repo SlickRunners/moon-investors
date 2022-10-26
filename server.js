3a56bc5ddff966130cd4e341916ea43e868b1e04
Roland Christensen <rchristensen@anonyome.com>
2022-10-26 09:44:05 -0600

    Update server.js port to work with hosted servers.

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 });
 
-app.listen(3000, function () {
-    console.log('Server started on port 3000')
+let port = process.env.port
+if (port == null || port == "") {
+    port = 3000;
+};
+
+app.listen(port, function () {
+    console.log('Server started succesfully')
 });
